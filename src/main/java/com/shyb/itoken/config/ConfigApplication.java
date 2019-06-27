bab5080c25d141cea1d505947bfc9bfd61eae188
17396387378 <515030185@qq.com>
2019-06-27 21:35:29 +0800

    配置init

---
+++ src/main/java/com/shyb/itoken/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.shyb.itoken.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wzh
+ * @date 2019/6/27 - 10:43
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
